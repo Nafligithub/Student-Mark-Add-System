45533259741945e72d508a656716a4fe7f7eac6b
Nafli <145882600+Nafligithub@users.noreply.github.com>
2026-05-20 15:15:17 +0530

    real to local

--- db.php
+++ db.php
@@ -1,5 +1,5 @@
 <?php
-$conn = new mysqli("sql108.infinityfree.com", "if0_41808369", "Nafly1234", "if0_41808369_nextgen_db");
+$conn = new mysqli("localhost", "root", "", "nextgen_db");
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
